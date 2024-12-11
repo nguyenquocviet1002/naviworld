91e53a169d1645d585a502aea1ad5cedfc741205
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-12-11 07:28:59 +0700

    'update'

---
+++ home/navi_1_0_0_slider/js/navi_1_0_0_slider.js
@@ -0,0 +1,12 @@
+var navi_1_0_0_slider = new Swiper(".navi_1_0_0_slider__mySwiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    dynamicMainBullets: 2,
+  },
+});
