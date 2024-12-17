cb75e07355ea3f72ef2fcd3404c3a8048375b56b
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-12-17 08:54:33 +0700

    update

--- home/navi_1_0_0_service/js/navi_1_0_0_service.js
+++ home/navi_1_0_0_service/js/navi_1_0_0_service.js
@@ -1,4 +1,7 @@
 var navi_1_0_0_service = new Swiper(".navi_1_0_0_service__mySwiper", {
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 30,
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
