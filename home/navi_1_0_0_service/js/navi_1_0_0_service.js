e90a99651c9a2b9eeeb9b7a6acea4a8f6243134f
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-12-17 07:23:54 +0700

    u

---
+++ home/navi_1_0_0_service/js/navi_1_0_0_service.js
@@ -0,0 +1,10 @@
+var navi_1_0_0_service = new Swiper(".navi_1_0_0_service__mySwiper", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
